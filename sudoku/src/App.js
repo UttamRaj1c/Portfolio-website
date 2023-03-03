372f39cb9f00b484f35c9ab3a9e0b6adb20e6778
UttamRaj1c <126783199+UttamRaj1c@users.noreply.github.com>
2023-03-03 14:22:07 +0530

    Add files via upload

---
+++ sudoku/src/App.js
@@ -0,0 +1,175 @@
+import './App.css';
+
+
+
+function App() {
+
+
+  return (
+
+    
+      <div>
+      <div className="container">
+
+      <div className="row">
+     <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+  <div className="col">
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1">  </div>
+      <div className="col1">  </div>
+    </div>
+    <div className="row1">
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+      <div className="col1"> </div>
+    </div>
+  </div> 
+</div>
+</div>
+ </div>
+    
+  );
+}
+
+export default App;
